22b03faf6c74a8f50363893db083b0459be238e8
Nikhil Tiwari <shraton1102@gmail.com>
2025-09-10 13:36:03 +0530

    Update userinfo.js

--- Frontend/js/userinfo.js
+++ Frontend/js/userinfo.js
@@ -1,1 +1,14 @@
+document.getElementById('userForm').addEventListener('submit', function(e){
+    e.preventDefault();
+    const age = document.getElementById('age').value;
+    const weight = document.getElementById('weight').value;
+    const health = document.getElementById('health').value;
+    const duration = document.getElementById('duration').value;
+    const motivation = document.getElementById('motivation').value;
+
+    const userInfo = {age, weight, health, duration, motivation};
+    localStorage.setItem('userInfo', JSON.stringify(userInfo));
+
+    window.location.href = 'treatment.html';
+});
 
